7d7fff9ae1c65b3169ebbc5227c77514a4cd2780
Artan.s <sun_kasehahk@hotmail.com>
2020-04-01 13:05:59 +0700

    CREATE VARIABLE TEMPLATE ALL my_constants

--- application/config/my_constants.php
+++ application/config/my_constants.php
@@ -17,8 +17,14 @@ define('LOG_WITH_USER', 2); //   errors
 define('TABLE_LOG','PCC_AUTHEN_LOG'); //Table Log
 //template
 
-define('TEMPLATE_WELCOME', 'template/TEMPLATE-HOME'); //  
-define('TEMPLATE_SHOP', 'template/TEMPLATE-SHOP'); // 
+define('TEMPLATE_WELCOME', 'template/TEMPLATE-HOME'); //  หน้าแรก
+define('TEMPLATE_SHOP', 'template/TEMPLATE-SHOP'); //   หน้า shop
+define('TEMPLATE_PRODUCT', 'template/TEMPLATE-PRODUCT'); //   หน้าสินค้า
+define('TEMPLATE_CART', 'template/TEMPLATE-CART'); // 
+define('TEMPLATE_BLOG', 'template/TEMPLATE-BLOG'); // 
+define('TEMPLATE_BLOGSINGLE', 'template/TEMPLATE-BLOGSINGLE'); // 
+define('TEMPLATE_CONTACT', 'template/TEMPLATE-CONTACT'); // 
+define('TEMPLATE_REGULAR', 'template/TEMPLATE-REGULAR'); //
 
 
 
